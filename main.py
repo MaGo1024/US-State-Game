99efff6eb87b6b7a307c5928a39a26f0d31ea07f
MaGo1024 <olumideodusote@gmail.com>
2026-02-25 00:42:35 +0000

    save unmentioned state to csv file.

--- main.py
+++ main.py
@@ -9,12 +9,22 @@ turtle.shape(image)
 
 data = pd.read_csv("50_states.csv")
 all_states = data.state.to_list()
-print(all_states)
+# print(all_states)
 guessed_states = []
 counter = 0
 
 while len(guessed_states) < len(all_states):
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct.", prompt="What's another state's name?").title()
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct.",
+                                    prompt="What's another state's name?").title()
+    if answer_state.title() == "Exit":
+        missing_state = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_state.append(state)
+        # print(len(missing_state))
+        new_data = pd.DataFrame(missing_state)
+        new_data.to_csv("states_to_learn.csv")
+        break
 
     if answer_state in all_states:
         guessed_states.append(answer_state)
@@ -24,8 +34,6 @@ while len(guessed_states) < len(all_states):
         state_data = data[data.state == answer_state]
         t.goto(state_data.x.item(), state_data.y.item())
         t.write(answer_state, font=("Arial", 9, "bold"))
-
-
 
 
 screen.exitonclick()
